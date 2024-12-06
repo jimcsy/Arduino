26868b1b164c1aa6a14e1d0bebbb5fdf5b6d93e2
jimcsy <165743267+jimcsy@users.noreply.github.com>
2024-12-06 08:20:38 +0800

    Delete Arduino directory

--- Arduino/Physics/src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Main {
-    public static void main(String[] args) {
-        // Initialize the ArduinoSerialReader object and start the GUI
-        ArduinoSerialReader arduinoSerialReader = new ArduinoSerialReader();
-        arduinoSerialReader.createAndShowGUI();
-    }
-}
